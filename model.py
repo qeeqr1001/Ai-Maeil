e34c41b72ae5badbf2551853576c79d9ac003e71
jhleezz00 <wldusdkwkd@gmail.com>
2022-07-12 02:42:31 +0900

    master

--- model.py
+++ model.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class NeuralNet(nn.Module):
+
     def __init__(self,input_size,hidden_size,num_classes):
-        super(NeuralNet,self).__init__()
+        super(NeuralNet, self).__init__()
         #3개의 선형레이어 만들기
         self.l1=nn.Linear(input_size,hidden_size) #input_size와 num_classes는 고정값, hidden_size는 변경할 수 있음
         self.l2=nn.Linear(hidden_size,hidden_size) 
